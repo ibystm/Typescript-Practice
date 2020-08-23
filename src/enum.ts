2793e05459aa3211fbefc6ecd51f3cdd2d0af285
Takahashi Kosuke <takahashikosuke@ystm.local>
2020-08-23 22:35:34 +0900

    add enum types

--- src/enum.ts
+++ src/enum.ts
@@ -1,1 +1,30 @@
 export {};
+
+enum Month {
+  January = 1,
+  Feburary,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+// enumで自動付与される先頭の数値は変えられる!
+
+enum Colors {
+  RED = "#FF0000",
+  WHITE = "#FFF",
+  GREEN = "#008000",
+  BLUE = "#0000FF",
+  BLACK = "#000000",
+}
+
+let green = Colors.GREEN;
+
+console.log(green);
